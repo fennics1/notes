8164a76b39b5bc09aedbac3d45669b821c147430
Fenix Shih <fennics1@gmail.com>
2019-03-29 15:48:17 +0800

    Mark selected note in list

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 import { Notes } from '../api/notes';
 import NoteListHeader from './NoteListHeader';
@@ -26,10 +27,17 @@ NoteList.propTypes = {
 };
 
 export default withTracker(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
+
   Meteor.subscribe('notes');
 
   return {
-    notes: Notes.find().fetch()
+    notes: Notes.find().fetch().map((note) => {
+      return {
+        ...note,
+        selected: note._id === selectedNoteId
+      };
+    })
   };
 
 })(NoteList);
